155fb27e3f6c6a250d88a6d25ab637ddbac2a4cf
hhhh48 <44383222+hhhh48@users.noreply.github.com>
2018-10-23 11:49:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,36 +27,5 @@ client.user.setGame(`Bot`,"")
   console.log('')
   console.log('')
 });
-// On déclare le préfixe
-var prefix = '$$';
-
-client.on("message", message => {
-
-  // Le robot lui répondra pong en le mentionnant si il envoie "ping"
-  if (message.content === "reply"){
-    // le robot répond "@auteur#1234, Salut l'ami !"
-    message.reply("Salut l'ami !");
-  };
-  // Le robot lui répondra pong si il envoie "ping"
-  if (message.content === "ping"){
-    // le robot répond "Pong !"
-    message.channel.send("Pong !");
-  };
-  // Le robot lui répondra ceci si un utilisateur fait "$$help"
-  if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("Vous avez fait `$$help` pour avoir l'aide.");
-  };
-  // Le robot lui répondra si l'utilisateur envoie "mp"
-  if (message.content == "mp"){
-    message.author.send("voici un message privé !")
-  };
-   // Le robot lui répondra ceci si un utilisateur envoie "?avatar"
-  if (message.content.startsWith(prefix + "avatar")) {
-    message.channel.send(message.author.avatarURL);
-  };
-
-})
-client.login("VOTRE TOKEN ICI SANS ESPACE");
-
 
 client.login(process.env.BOT_TOKEN);
