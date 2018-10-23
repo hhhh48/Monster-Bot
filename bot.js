046d49712661b6be565feef7efee32f7276fc4a7
hhhh48 <44383222+hhhh48@users.noreply.github.com>
2018-10-23 10:05:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,33 +27,5 @@ client.user.setGame(`$$Help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix $$ "help") {
 
-
- message.author.sendMessage(`
- 
-
- ╱╭╮╭╮╱╱╱╱╭╮╭━╮╱╱╱╱╱╱╭━━╮╱╱╱
-♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ 
-اوامر البوت
-❖اوامر عامه
-
-❖!link  لاخذ رابط اي بوت 
-❖!رابط لاخذ رابط السيرفر
-❖!invites لمعرفة كم دعوت شخص
-❖ !server  لمعرفت معلومات السيرفر
-❖!bans  يعطيك تاقات الي تبندو ورا بعض 
-❖!owner لترسل رسالة لصاحب البوت
-❖!مسح لمسح الشات 
-❖!اقتراح وكتب اقتراحك وسوف يصل الاداره
-❖!move لسحب جميع الاعضاء الذين في الرومات لعندك
-❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖❖
-❖اوامر العاب 
-❖!زواج لعبة زواج
-❖!ابلع  لعبة حلوه
-
-    }
-});
 client.login(process.env.BOT_TOKEN);
