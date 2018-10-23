ec0132eee6c9fbf69689b1411d7fa36894b1b63e
hhhh48 <44383222+hhhh48@users.noreply.github.com>
2018-10-23 10:59:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,33 @@ client.user.setGame(`Bot`,"")
   console.log('')
   console.log('')
 });
+client.on("message", message => {
+ if (message.content === "-help") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© Premium Bot:heart: جميع الحقوق محفوظة 2017 لــبوت')
+      .setFooter('اوامر الادمنيه')
+	  .addField('$$bc', `لخاصية البرودكستات`)
+      .setFooter('======================================================')
+      .setFooter('اوامر الاعضاء')
+      .addField('$$invite', `لاضافة البوت الى سيرفرك`)
+	  .addField('$$roles', `لمعرفة الرتب الي في السيرفر`)
+      .addField('$$avatar', `يجبلك الافتار حقك يعني صورة حسابك`)
+      .addField('$$2avatar', `الافتار عن طريق المنشن`)
+	  .addField('$$server', `يجبلك معلومات السيرفر`)
+	  .addField('$$id', `يجبلك الملف الشخصي حقك`)
+	  .addField('$$clear', `البوت يمسح  100 رسايل`)
+	  .addField('$$say', `البوت يكرر الكلام الي انت تقوله`)
+	  .addField('$$ping', `يقلك كم بنق البوت`)
+      .addField('$$support', `سيرفر الدعم`)
+      .setFooter('======================================================')
+      .setFooter('اوامر تحت الصيانه(يمكنك تجربتها) وشكرا')
+      .addField('$$play', `لتسمع الاغنيه`)
+      .addField('$$غرد', `للتغريد`)
+      .addField('$$embed', `البوت يكرر الكلام الي قلته ب امبد`)
+	  .addField('$$cat', `يجبلك صورة بسه`)
+  message.author.send({embed});
 
+ }
+});
 client.login(process.env.BOT_TOKEN);
